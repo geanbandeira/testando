57f71dfdafeec66a69c29e62b4d8404c3efe0a02
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2023-09-05 11:57:05 -0300

    Create loja.js

---
+++ loja.js
@@ -0,0 +1,44 @@
+// Objeto que representa um Produto
+function Produto(nome, preco) {
+  this.nome = nome;
+  this.preco = preco;
+}
+
+// Método para calcular o preço total do produto
+Produto.prototype.calcularPrecoTotal = function() {
+  return this.preco;
+};
+
+// Objeto que representa um CarrinhoDeCompras composto por produtos
+function CarrinhoDeCompras() {
+  this.produtos = [];
+}
+
+// Método para adicionar um produto ao carrinho
+CarrinhoDeCompras.prototype.adicionarProduto = function(produto) {
+  this.produtos.push(produto);
+};
+
+// Método para calcular o preço total do carrinho
+CarrinhoDeCompras.prototype.calcularPrecoTotal = function() {
+  let total = 0;
+  for (const produto of this.produtos) {
+    total += produto.calcularPrecoTotal();
+  }
+  return total;
+};
+
+// Criando produtos
+const produto1 = new Produto('Camiseta', 20);
+const produto2 = new Produto('Calça', 40);
+
+// Criando um carrinho de compras
+const carrinho = new CarrinhoDeCompras();
+
+// Adicionando produtos ao carrinho
+carrinho.adicionarProduto(produto1);
+carrinho.adicionarProduto(produto2);
+
+// Calculando o preço total do carrinho
+const precoTotal = carrinho.calcularPrecoTotal();
+console.log(`Preço total do carrinho: $${precoTotal}`);
