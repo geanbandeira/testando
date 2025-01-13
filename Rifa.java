85ce284a3f73915658dfb7f5f5ab53e3eb18c2a9
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2025-01-13 20:56:03 -0300

    Create Rifa.java

---
+++ Rifa.java
@@ -0,0 +1,10 @@
+@Entity
+public class Rifa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+    private int quantidade;
+}
